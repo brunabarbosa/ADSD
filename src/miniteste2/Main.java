da02ffc74150ea32dc72a9088e3f50a5393b3270
bruna <bruna-mac@brunas-MacBook-Pro.local>
2017-12-21 19:07:12 -0300

    sim_trace

--- src/miniteste2/Main.java
+++ src/miniteste2/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Sim_system.initialise();
+		Sim_system.set_trace_detail(true, true, true);
 	    Sim_system.add(new Morador("Sender", 1, Morador.SRC_OK));
 	    Sim_system.add(new Sindico("Receiver", 2, Sindico.SINK_OK));
 	    Sim_system.link_ports("Sender", "out", "Receiver", "in");
